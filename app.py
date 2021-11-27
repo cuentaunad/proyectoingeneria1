3157a0c04dbb6bbbf60da1f996d4e20eaf179f50
henry.carrillo <henry.carrillo@geniussports.com>
2021-11-27 10:44:21 -0500

    update

---
+++ app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("Sistema de control de mercancias")
---
+++ setup.sh
@@ -0,0 +1,27 @@
+# Copyright 2021 henry.carrillo
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"email@domain\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
